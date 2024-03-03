78450131e8d3a117db58fe747ad8cdf15a156c1f
jannet18 <janetngei18@gmail.com>
2024-03-03 13:06:20 +0300

    dashboard router

--- server/routes/General.js
+++ server/routes/General.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getUser } from "../controllers/General.js";
+import { getDashboardStats, getUser } from "../controllers/General.js";
 
 const router = express.Router();
 router.get("/user/:id", getUser);
-
+router.get("/dashboard", getDashboardStats);
 export default router;
