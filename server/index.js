9a82daad90d0e1237bd8200a9793f1b06f581171
jannet18 <janetngei18@gmail.com>
2024-03-04 16:58:12 +0300

    path.join bracket moved

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const PORT = process.env.PORT || 9000;
 const __dirname = path.resolve();
 app.use(express.static(__dirname));
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname), "../client"));
+  app.use("/", express.static(path.join(__dirname, "../client")));
 
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "/public/index.html"));
