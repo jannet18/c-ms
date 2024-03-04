b2688a1278bd9b58d3a1b93f22cef005508e8748
jannet18 <janetngei18@gmail.com>
2024-03-04 16:39:40 +0300

    deployment configuration

--- server/index.js
+++ server/index.js
@@ -50,10 +50,10 @@ const PORT = process.env.PORT || 9000;
 const __dirname = path.resolve();
 app.use(express.static(__dirname));
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname), "../client"));
+  app.use("/*", express.static(path.join(__dirname)));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "/public/index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
   });
 }
 
